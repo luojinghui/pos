a9b9b4124e949dbd86510d2f409dcc4dd80f9d98
luojinghui <939035243@qq.com>
2015-05-19 16:58:21 +0800

    finsh pos_v.0

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -1,4 +1,14 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
-
+    var shop_list = '***<没钱赚商店>购物清单***';
+    var sum = 0;
+    for(var i = 0 ;i < inputs.length;i ++){
+        shop_list = shop_list +'\n'+'名称：'+inputs[i].name+'，数量：'+inputs[i].count+
+        inputs[i].unit+'，单价：'+inputs[i].price+'.00(元)，小计：'+
+        inputs[i].price*inputs[i].count+'.00(元)';
+        sum = sum+(inputs[i].price*inputs[i].count);
+    }
+    shop_list = shop_list +'\n----------------------\n'+
+    '总计：'+sum+'.00(元)'+'\n'+'**********************';
+    console.log(shop_list);
 }
