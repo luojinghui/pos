74ad52b891a9a565efb547f0cee532a867ef7995
luojinghui <939035243@qq.com>
2015-05-20 15:45:47 +0800

    clean pos0 and pos0.1 code

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,14 +1,14 @@
-//TODO: Please write code in this file.
+
 function printInventory(inputs) {
     var array = [];
-    for(var i = 0; i < inputs.length; i++){
+    for(var i=0 ; i<inputs.length; i++){
         var exist = false;
         var name = inputs[i].name;
         var count = 1;
         var price = inputs[i].price;
         var unit = inputs[i].unit;
-        for(var j = 0; j < array.length; j++){
-            if(array[j].name===name){
+        for(var j=0 ; j<array.length ; j++){
+            if(array[j].name === name){
                 array[j].count = array[j].count + 1;
                 array[j].price = inputs[i].price;
                 array[j].unit = inputs[i].unit;
@@ -17,22 +17,30 @@ function printInventory(inputs) {
         }
         if(!exist){
             var temp = {};
-            temp.name = name;
+            temp.name  = name;
             temp.count = count;
             temp.price = price;
-            temp.unit = unit;
+            temp.unit  = unit;
             array.push(temp);
         }
     }
-    var total_note = "***<没钱赚商店>购物清单***"+ '\n';
-    var sum =0;
-    sum = inputs[0].price*array[0].count+inputs[5].price*array[1].count+inputs[7].price*array[2].count;
-        for(var x = 0;x < array.length; x++){
-            total_note = total_note  +'名称：'+array[x].name+'，数量：'+array[x].count+
-            array[x].unit+'，单价：'+array[x].price+'.00(元)，小计：'+array[x].price*array[x].count+
-            '.00(元)'+'\n';
-    }
-            total_note = total_note + '----------------------'+'\n'+
-            '总计：'+sum+'.00(元)'+'\n'+'**********************';
+
+    var total_note = "***<没钱赚商店>购物清单***" + '\n';
+    var sum = 0;
+    var subtotal;
+        for(var x=0 ; x<array.length ; x++){
+            sum += array[x].price * array[x].count ;
+            subtotal = array[x].price * array[x].count;
+            total_note = total_note  +
+                        '名称：' + array[x].name +
+                        '，数量：' + array[x].count + array[x].unit +
+                        '，单价：' + array[x].price.toFixed(2) +'(元)，小计：' +
+                        subtotal.toFixed(2)+'(元)' + '\n';
+            }
+
+            total_note = total_note +
+                        '----------------------' +'\n' +
+                        '总计：'+ sum.toFixed(2) +'(元)' +'\n' +
+                        '**********************';
     console.log(total_note);
 }
--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -2,13 +2,20 @@
 function printInventory(inputs) {
     var shop_list = '***<没钱赚商店>购物清单***';
     var sum = 0;
-    for(var i = 0 ;i < inputs.length;i ++){
-        shop_list = shop_list +'\n'+'名称：'+inputs[i].name+'，数量：'+inputs[i].count+
-        inputs[i].unit+'，单价：'+inputs[i].price+'.00(元)，小计：'+
-        inputs[i].price*inputs[i].count+'.00(元)';
-        sum = sum+(inputs[i].price*inputs[i].count);
-    }
-    shop_list = shop_list +'\n----------------------\n'+
-    '总计：'+sum+'.00(元)'+'\n'+'**********************';
+    var subtotal = 0;
+    for(var i=0 ; i<inputs.length ; i++){
+        subtotal = inputs[i].price * inputs[i].count;
+        var unit = inputs[i].count + inputs[i].unit;
+        sum = sum + (inputs[i].price * inputs[i].count);
+        shop_list = shop_list + '\n' +
+                    '名称：' + inputs[i].name +
+                    '，数量：' + unit +
+                    '，单价：' + inputs[i].price.toFixed(2) + '(元)，小计：' +
+                    subtotal.toFixed(2) + '(元)';
+            }
+    shop_list = shop_list +
+                    '\n----------------------\n'+
+                    '总计：' + sum.toFixed(2) + '(元)' + '\n' +
+                    '**********************';
     console.log(shop_list);
 }
